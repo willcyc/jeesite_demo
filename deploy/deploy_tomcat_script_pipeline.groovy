8d197f688b931fdc70666c38e7fe7c62815dbc15
willcyc <13839205941@163.com>
2021-03-17 21:02:29 +0800

    Update deploy_tomcat_script_pipeline.groovy

--- deploy/deploy_tomcat_script_pipeline.groovy
+++ deploy/deploy_tomcat_script_pipeline.groovy
@@ -68,7 +68,7 @@ node('master') {
         sh '''
             JENKINS_NODE_COOKIE=dontkillme
             BUILD_ID=dontKillMe
-            BUILD_ID=1
+            env.JENKINS_NODE_COOKIE="dontKillMe"
             cd $tomcat_home/bin
             sh startup.sh
         '''
